e152dd91c9065142490a3ce7853976e54a92d4e3
Nicolo Guerra <nicolo.guerra@studio.unibo.it>
2025-03-26 12:43:58 +0100

    Implemented thread per boid version

--- src/main/java/pcd/ass01/BoidsSimulator.java
+++ src/main/java/pcd/ass01/BoidsSimulator.java
@@ -1,6 +1,7 @@
 package pcd.ass01;
 
 import java.util.Optional;
+import java.util.concurrent.CyclicBarrier;
 
 public class BoidsSimulator {
 
@@ -20,45 +21,64 @@ public class BoidsSimulator {
     }
       
     public void runSimulation() {
+        var boids = model.getBoids();
+        final CyclicBarrier barrier = new CyclicBarrier(boids.size()+1);
+
+        for (Boid boid : boids) {
+            Thread.ofVirtual().start(() -> {
+                while (true) {
+                    BoidsModel localModel = new BoidsModel(model);
+                    boid.updateVelocity(localModel);
+                    try {
+                        barrier.await();
+                    } catch (Exception ex) {
+                        ex.printStackTrace();
+                    }
+                    boid.updatePos(localModel);
+                    try {
+                        barrier.await();
+                    } catch (Exception ex) {
+                        ex.printStackTrace();
+                    }
+                }
+            });
+        }
+
+        var t0 = System.currentTimeMillis();
     	while (true) {
-            var t0 = System.currentTimeMillis();
-    		var boids = model.getBoids();
-    		/*
-    		for (Boid boid : boids) {
-                boid.update(model);
-            }
-            */
-    		
-    		/* 
-    		 * Improved correctness: first update velocities...
-    		 */
-    		for (Boid boid : boids) {
-                boid.updateVelocity(model);
-            }
-
-    		/* 
-    		 * ..then update positions
-    		 */
-    		for (Boid boid : boids) {
-                boid.updatePos(model);
-            }
-
             
-    		if (view.isPresent()) {
-            	view.get().update(framerate);
+            // Threads are updating velocities
+            if (view.isPresent()) {
+                view.get().update(framerate);
             	var t1 = System.currentTimeMillis();
                 var dtElapsed = t1 - t0;
                 var framratePeriod = 1000/FRAMERATE;
                 
                 if (dtElapsed < framratePeriod) {		
-                	try {
-                		Thread.sleep(framratePeriod - dtElapsed);
+                    try {
+                        Thread.sleep(framratePeriod - dtElapsed);
                 	} catch (Exception ex) {}
                 	framerate = FRAMERATE;
                 } else {
-                	framerate = (int) (1000/dtElapsed);
+                    framerate = (int) (1000/dtElapsed);
                 }
     		}
+            
+            t0 = System.currentTimeMillis();
+            
+            // Wait for threads to update velocities
+            try {
+                barrier.await();
+            } catch (Exception ex) {
+                ex.printStackTrace();
+            }
+            
+            // Wait for threads to update positions
+            try {
+                barrier.await();
+            } catch (Exception ex) {
+                ex.printStackTrace();
+            }
             
     	}
     }
