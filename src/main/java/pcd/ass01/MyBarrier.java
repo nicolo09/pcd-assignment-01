9a49c80046e99b05537c92a8ad36da5becff866b
Nicolo Guerra <nicolo.guerra@studio.unibo.it>
2025-04-01 11:04:54 +0200

    Changed barrier layer type to object

--- src/main/java/pcd/ass01/MyBarrier.java
+++ src/main/java/pcd/ass01/MyBarrier.java
@@ -1,29 +1,33 @@
 package pcd.ass01;
 
-import java.util.concurrent.locks.Lock;
-import java.util.concurrent.locks.ReentrantLock;
-
 public class MyBarrier {
 
     private volatile int count = 0;
     private final int totalThreads;
-    private volatile Lock lock = new ReentrantLock();
+    private volatile Object layer = new Object();
 
     public MyBarrier(int totalThreads) {
         this.totalThreads = totalThreads;
     }
 
+    /**
+     * This method is called by each thread to wait at the barrier.
+     * It will block until all threads have called this method.
+     * After all threads have called this method, they will be released.
+     *
+     * @throws InterruptedException if the current thread is interrupted while waiting
+     */
     public void await() throws InterruptedException {
-        final Lock localLock = this.lock; 
-        synchronized(this){
+        final var localLayer = this.layer;
+        synchronized (this) {
             count++;
             if (count == totalThreads) {
                 count = 0; // Reset for next use
-                this.lock = new ReentrantLock(); // Create a new lock for the next barrier
+                this.layer = new Object(); // Create a new lock for the next barrier
                 this.notifyAll(); // Notify all waiting threads
             } else {
-                while (localLock == this.lock) { // Wait until the barrier is released
-                    this.wait(); // Wait until all threads reach the barrier     
+                while (localLayer == this.layer) { // Wait until the barrier is released
+                    this.wait(); // Wait until all threads reach the barrier
                 }
             }
         }
